6198200f2ecf42af2e11ddad6921e2651661d054
Alexis Cañez <75396512+RoastedPeanuts806@users.noreply.github.com>
2021-04-26 00:10:37 -0700

    Update functionality
    Got rid of Button device. In the end, the only part of this code that will remain is the function.

--- relay.py
+++ relay.py
@@ -1,26 +1,27 @@
 # set up relays
-from gpiozero import Button, OutputDevice
+from gpiozero import OutputDevice
+import time
+import sys
 
 # devices specified by GPIO number
-air_pump = OutputDevice(18)
-nutrient_pump = OutputDevice(23)
-solenoid = OutputDevice(24)
-l_board = OutputDevice(25)
-
-# pressing button will toggle airpump power
-button = Button(8)
+# gpiozero.OutputDevice(pin, *, active_high=True, initial_value=False, pin_factory=None)
+air_pump = OutputDevice(18, False)
+nutrient_pump = OutputDevice(23, False)
+solenoid = OutputDevice(24, False)
+l_board = OutputDevice(25, False)
+motors = OutputDevice(4, False)
 
 #function to toggle relay power
 def TogglePower(relay):
-    toggle(relay)
+    relay.toggle()
 
-
-
-while True:
-    try:
-        if button.is_pressed:
-            TogglePower(air_pump)
-    except KeyboardInterrupt:
-        exit()
-        
-
+#TogglePower(air_pump)
+        #time.sleep(1)
+#TogglePower(motors)
+        #time.sleep(1)
+#TogglePower(l_board)
+        #time.sleep(1)
+#TogglePower(nutrient_pump)   
+#TogglePower(solenoid)
+#time.sleep(1)
+#TogglePower(solenoid)
